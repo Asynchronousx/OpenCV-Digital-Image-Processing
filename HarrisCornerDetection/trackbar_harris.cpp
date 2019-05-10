dfd078c616444d9873b961513e267a0b6a867678
Asynchronousx <38207760+Asynchronousx@users.noreply.github.com>
2019-05-10 19:21:30 +0200

    Update trackbar_harris.cpp

--- HarrisCornerDetection/trackbar_harris.cpp
+++ HarrisCornerDetection/trackbar_harris.cpp
@@ -411,6 +411,9 @@ void slideHarris(int , void*) {
 			   corners.at(j).y <= corners.at(i).y+pad && corners.at(j).y >= corners.at(i).y-pad) {
 			   		//if the condition is met, erase the pixel
 			   		corners.erase(corners.begin() + j);
+					//decrement the j index: that's because we're still into the J for and we need to inform
+			   		//the cycle we decremented the index by erasing.
+			   		j--;
 			   }
 		}			
 	}
